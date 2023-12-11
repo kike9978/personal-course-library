405bc396bafa1429000ba9d29af2bbefc67041c4
Kike Mendoza <kike.mendoza9978@gmail.com>
2023-12-11 11:58:00 -0600

    Render local image

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,5 +1,10 @@
 import CoursesGrid from "./components/CoursesGrid"
+// import { IpcRenderer, ipcRenderer } from "electron"
 import { useState } from 'react'
+
+// function getNativeImageData(pathToImage) {
+//   return ipcRenderer.invoke("getNativeImage", pathToImage);
+// }
 
 function FilterableCoursesGrid({ courses }) {
   const [filterText, setFilterText] = useState('')
